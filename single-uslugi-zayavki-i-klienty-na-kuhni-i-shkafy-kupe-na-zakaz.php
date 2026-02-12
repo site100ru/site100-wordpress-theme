927c7cbb6862f5a9be49ac7f0da19f0e077c1aef
Roman <150604774+vasilyev-r@users.noreply.github.com>
2026-02-12 16:30:26 +0300

    About section (#16)
    * Update about section
    
    * Update about section

--- single-uslugi-zayavki-i-klienty-na-kuhni-i-shkafy-kupe-na-zakaz.php
+++ single-uslugi-zayavki-i-klienty-na-kuhni-i-shkafy-kupe-na-zakaz.php
@@ -656,7 +656,7 @@ if (isset($_SESSION['win'])) {
 	<section class="about bg-light">
 		<div class="container">
 			<div class="row">
-				<div class="col text-md-center">
+				<div class="col">
 					<h2 class="text-start section-title text-md-center mb-3">Обо мне</h2>
 					<img src="<?php echo get_template_directory_uri(); ?>/img/ico/section-title-decoration.svg" class="mb-5">
 					<div class="row align-items-center">
